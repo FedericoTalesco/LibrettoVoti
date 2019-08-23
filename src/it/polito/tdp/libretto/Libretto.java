219b1523555d24190b65b9b2a16d2169f9f3188a
FedericoTalesco <talescofederico@gmail.com>
2019-08-23 09:55:51 +0200

    progetto iniziale. Classe voto inizializzata

---
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -0,0 +1,17 @@
+package it.polito.tdp.libretto;
+
+import java.util.*;
+
+public class Libretto {
+
+	private List<Voto> voti;
+	//qunado possibile mi tengo sul più generico possibile
+	
+	public Libretto() {
+		this.voti= new ArrayList<Voto>();//qui devo per forza specificare nella parte iniziale posso usare solo l'interfaccia
+		}
+	public void add(Voto v) {
+		
+	}
+	
+}
